f015b5403579d5dba2384891a666d4f357ae36bf
gaomingzhao-system <m-kou@mldfp.com>
2025-09-27 12:00:52 +0900

    fix: footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   const year = date.getFullYear();
 
   return (
-    <footer className="w-full border-t rounded-3xl bg-white/50 px-4 py-8 text-center text-sm text-gray-600 backdrop-blur-sm">
+    <footer className="fixed z-50 bottom-0 w-full ring rounded-t-3xl  bg-white/50 dark:bg-black/50  px-4 py-8 text-center text-sm text-gray-600 backdrop-blur">
       <p>© {year} Your Corporation Name</p>
     </footer>
   );
