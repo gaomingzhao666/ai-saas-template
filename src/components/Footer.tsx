484e3f21d92320bcd8105dd42b1d159b9f92d3aa
gaomingzhao-system <m-kou@mldfp.com>
2025-09-27 16:10:08 +0900

    chore: use border instead of ring to fix dark mode compatiblity problem

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   const year = date.getFullYear();
 
   return (
-    <footer className="w-full mt-12 ring rounded-t-3xl bg-white/50 dark:bg-black/50 px-4 py-8 text-center text-sm text-gray-600 backdrop-blur">
+    <footer className="w-full mt-12 border-t rounded-t-3xl bg-white/50 dark:bg-black/50 px-4 py-8 text-center text-sm text-gray-600 backdrop-blur">
       <p>© {year} Your Corporation Name</p>
     </footer>
   );
