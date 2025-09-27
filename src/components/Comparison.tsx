797ef3535d62a8f3692a9a7206289c3170424194
gaomingzhao-system <m-kou@mldfp.com>
2025-09-27 14:16:20 +0900

    feat: Comparison section

---
+++ src/components/Comparison.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Compare } from "@/components/ui/compare";
+
+export function Comparison() {
+  return (
+    <Compare
+      firstImage="https://assets.aceternity.com/code-problem.png"
+      secondImage="https://assets.aceternity.com/code-solution.png"
+      firstImageClassName="object-cover object-left-top"
+      secondImageClassname="object-cover object-left-top"
+      className="h-[32rem] w-full md:w-[64rem] md:h-[32rem] mx-auto"
+      slideMode="hover"
+    />
+  );
+}
