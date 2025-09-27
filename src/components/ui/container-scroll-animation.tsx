fa91741d8eccf68b9fcb2008b058ccfdf34d9a28
gaomingzhao-system <m-kou@mldfp.com>
2025-09-27 13:44:12 +0900

    fix: styles

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -41,7 +41,7 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[48rem] md:h-[64rem] flex items-center justify-center relative p-2 md:p-20"
+      className="h-[48rem] md:h-[64rem] flex items-center justify-center relative p-2 md:p-16"
       ref={containerRef}
     >
       <div
