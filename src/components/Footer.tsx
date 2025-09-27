59d7ea0169d21d8fc43976ae6c2ac4b457301846
gaomingzhao-system <m-kou@mldfp.com>
2025-09-27 11:41:17 +0900

    chore: tiny style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   const year = date.getFullYear();
 
   return (
-    <footer>
+    <footer className="w-full border-t rounded-3xl bg-white/50 px-4 py-8 text-center text-sm text-gray-600 backdrop-blur-sm">
       <p>© {year} Your Corporation Name</p>
     </footer>
   );
