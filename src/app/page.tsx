e8985662a2caf28a2473eea4593ec81fff64f117
gaomingzhao-system <m-kou@mldfp.com>
2025-09-27 11:26:40 +0900

    Blog page occupations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+import { Footer } from "@/components/Footer";
 import { HeroScroll } from "@/components/Hero";
 import { Members } from "@/components/Members";
 import { HomeNavbar } from "@/components/Navbar";
 
 const Home = () => (
   <>
-    <HomeNavbar />
+    <header>
+      <HomeNavbar />
+    </header>
+
     <HeroScroll />
     <Members />
 
     <div className="fixed bottom-2 right-2">AI ChatBot</div>
+
+    <Footer />
   </>
 );
 
